59a262851cbb789db158d9920b5c12f5b46d6ed3
flapperz <ashishkerion@gmail.com>
2023-02-06 01:13:15 -0400

    finished users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -66,7 +66,7 @@ exports.users_signup = function(req,res,next)   //Sign up post request.
                 Userclass:req.body.Userclass,
                 Firstname:req.body.Firstname,
                 Lastname:req.body.Lastname,
-                Requestedrole:req.body.Requestedrole,
+                Agencytype:req.body.Agencytype,
                 Password:hash,
                 Email:req.body.Email,
                 DateJoined:req.body.DateJoined
@@ -88,4 +88,92 @@ exports.users_signup = function(req,res,next)   //Sign up post request.
 }
     });
   });
+}
+
+exports.users_login = (req,res,next)=>{
+    User_model.find({Username: req.body.Username,Userclass:req.body.Userclass})
+
+    .exec()
+    .then(user=>{
+
+        if (user.length<1){
+            return res.status(401).json({
+                message:'Authorization failed'
+            });
+        }
+
+        bcrypt.compare(req.body.Password, user[0].Password,(err,result)=>{
+            if(err){
+                return res.status(401).json({
+                    message:'Authorization Failed'
+                });
+            }
+            if (result){
+                const token = jwt.sign(
+               {
+                    Username: user[0].Username,
+                    _id:user[0]._id,
+                    Userclass:user[0].Userclass,
+                    Firstname:user[0].Firstname,
+                    Lastname:user[0].Lastname
+                },
+                "secret",
+                {
+                    expiresIn: "10h"
+                }
+                );
+
+                return res.status(200).json({
+                    message:"Authorization sucessful",
+                    token: token
+                });
+            }
+            res.status(401).json({
+                message:"Authorization Failed"
+            });
+        });
+
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+    });
+}
+
+exports.update_user = function(req,res,next)
+{
+    const id = req.params.updateUser;
+    User_model.updateOne({_id: id},{$set:{Username:req.body.Username,Userclass:req.body.Userclass,Firstname:req.body.Firstname,Lastname:req.body.Lastname,Email:req.body.Email,Password:req.body.Password,Requestedrole:req.body.Requestedrole}})
+    .exec()
+    .then(function(dbuser)
+      {
+
+          res.send(dbuser);
+      })
+      .catch(function(err){
+          res.send('Cannot update form');
+      });
+
+}
+
+
+
+exports.delete_user = function(req,res,next)
+{
+    User_model.deleteOne({_id:req.params.userId})
+    .exec()
+    .then(result=>{
+        console.log(result);
+      res.status(200).json({
+          message:"The User is deleted"
+      })
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+    });
 }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,3 +8,20 @@ const Admin_model= require('../DataModels/Admins.model')
 const UsersController = require ('../controller/users')
 const checkAuth = require ('../api/middleware/check-auth')
 
+
+
+
+router.get('/list',UsersController.users_get_list );
+
+router.get ('/userchart',UsersController.getuserschart)
+
+
+router.get("/:id",UsersController.users_get_one);  //Find user based on ID
+
+
+router.post('/signup',UsersController.users_signup); //Registration 
+
+
+router.post('/login',UsersController.users_login) //User login
+
+
