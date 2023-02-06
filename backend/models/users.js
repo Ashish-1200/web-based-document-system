d70b24e9d923a3f36d5aa76d3af1ea0f75b87d74
flapperz <ashishkerion@gmail.com>
2023-02-06 01:09:09 -0400

    updated code

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,4 +1,60 @@
 const mongoose = require("mongoose")
 const bcrypt = require('bcrypt')
-const Schema = mongoose.Schema;
+
+const usersSchema= mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+
+    Username:{
+        type:String,
+        unique: true,
+        required:true,
+    },
+    Userclass: {
+        type: String,
+
+    },
+    Agencytype:{
+      type:String,
+    },
+    Firstname: {
+        type:String,
+        required:true,
+    },
+    Lastname:{
+        type:String,
+        required:true,
+    },
+
+    Password: {
+        type:String,
+        required:true,
+        validate(pass){
+            console.log(pass)
+            if(pass.length < 8){
+                throw new Error("Password must be 8 or more characters ")
+            }
+        }
+    },
+    Email: {
+        type:String,
+        unique:true,
+        required: true,  // Email address should be entered
+        lowercase:true,  // Emails should be lowercase
+        validate: {
+            validator: function(v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email"
+        },
+        required: [true, "Email required"]
+
+    },
+
+    DateJoined: {
+        type: Date,
+        default: new Date(),
+    } ,
+})
+
+module.exports =mongoose.model("users",usersSchema)
 
